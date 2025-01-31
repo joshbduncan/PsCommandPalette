181e89ab6d586849e3d50bfb8b224b30a5a9ab7a
Josh Duncan <44387852+joshbduncan@users.noreply.github.com>
2025-01-31 00:25:17 -0500

    plugin dockable palette, user data updates

--- main.js
+++ main.js
@@ -1,39 +1,92 @@
 const { app } = require("photoshop");
 const { core } = require("photoshop");
-const { entrypoints } = require("uxp");
+const uxp = require("uxp");
+const { entrypoints } = uxp;
 
 const { alertDialog } = require("./src/dialogs/alert.js");
 const { User } = require("./src/User.js");
 const { CommandData } = require("./src/commands/CommandData.js");
 const { CommandPalette } = require("./src/CommandPalette.js");
 
-// get plugin info
+/////////////////////
+// get plugin info //
+/////////////////////
 const manifest = require("./manifest.json");
 const PLUGIN_NAME = manifest.name;
 const PLUGIN_VERSION = manifest.version;
+const PLUGIN_AUTHOR = manifest.author;
+const HOST_NAME = uxp.host.name;
+const HOST_VERSION = uxp.host.version;
+console.log("loading plugin:", PLUGIN_NAME, `v${PLUGIN_VERSION}`);
 
-// setup data objects
+/////////////////////////
+// create data objects //
+/////////////////////////
 const USER = new User();
 const COMMAND_DATA = new CommandData();
-
-console.log("loading plugin:", PLUGIN_NAME, `v${PLUGIN_VERSION}`);
 
 entrypoints.setup({
   commands: {
     launchPalette: () => launchPalette(),
     reloadPlugin: () => reloadPlugin(),
   },
+  panels: {
+    ps_command_palette: {
+      show() {
+        // put any initialization code for your plugin here.
+      },
+      menuItems: [
+        { id: "launchPalette", label: "Open Command Palette" },
+        { id: "reloadPlugin", label: "Reload Plugin" },
+      ],
+      invokeMenu(id) {
+        switch (id) {
+          case "launchPalette":
+            launchPalette();
+            break;
+          case "reloadPlugin":
+            reloadPlugin();
+            break;
+        }
+      },
+    },
+  },
 });
 
-reloadPlugin = () => {
-  console.log("reloading ps-command-palette");
-  window.location.reload();
-};
+/////////////////////////
+// add main panel info //
+/////////////////////////
+const year = new Date().getFullYear();
+document.getElementById(
+  "main-copyright"
+).textContent = `Copyright ${year} ${PLUGIN_AUTHOR}`;
+
+document.getElementById(
+  "main-plugin-info"
+).textContent = `Plugin Version ${PLUGIN_VERSION}`;
+
+////////////////////////////////////
+// add main panel event listeners //
+////////////////////////////////////
+
+document
+  .getElementById("btnOpenCommandPalette")
+  .addEventListener("click", launchPalette);
+
+document.getElementById("btnReloadPlugin").addEventListener("click", reloadPlugin);
+
+///////////////////////
+// command functions //
+///////////////////////
 
 async function launchPalette() {
   // load user data
-  await USER.load();
-  console.log(USER.data);
+  try {
+    await USER.load();
+    console.log(USER.data);
+  } catch (error) {
+    console.log(error);
+  }
 
   // load palette commands
   await COMMAND_DATA.loadCommands();
@@ -75,4 +128,12 @@ async function launchPalette() {
     // TODO: add alert - https://developer.adobe.com/photoshop/uxp/2022/design/ux-patterns/messaging/
     console.log("palette error:", error);
   }
+}
+
+/**
+ * Reload the plugin.
+ */
+function reloadPlugin() {
+  console.log("reloading ps-command-palette");
+  window.location.reload();
 }
--- src/User.js
+++ src/User.js
@@ -7,14 +7,17 @@ const { alertDialog } = require("./dialogs/alert.js");
  */
 class User {
   /**
-   * Create a UserData object.
+   * Create a User object.
    */
   constructor() {
     this.data = {};
-    this.fileName = "userData.json";
+    this.fileName = "pscp_user_data.json";
     this.file;
   }
 
+  /**
+   * Return a default object of sample user data.
+   */
   get defaultData() {
     // FIXME: temp user data for testing
     return {
@@ -22,11 +25,21 @@ class User {
         name: PLUGIN_NAME,
         version: PLUGIN_VERSION,
       },
+      app: {
+        name: HOST_NAME,
+        version: HOST_VERSION,
+      },
       history: [],
+      // TODO: add default builtin startup commands with about, docs, etc.
+      // TODO: should unavailable startup commands be shown but disabled or hidden completely?
       startupCommands: ["ps_menu_1030", "ps_menu_15204", "ps_menu_101"],
     };
   }
 
+  /**
+   * Load the user data file.
+   * @returns {Object}
+   */
   async load() {
     // https://developer.adobe.com/xd/uxp/develop/reference/uxp/module/storage/
 
@@ -36,59 +49,144 @@ class User {
     // reset current data
     this.data = {};
 
-    let f;
+    let f = this.file;
+
+    // if file property is undefined, create the an entry
+    if (f === undefined) {
+      try {
+        f = await dataFolder.createEntry(this.fileName, {
+          type: storage.types.file,
+          overwrite: true,
+        });
+        this.file = f;
+      } catch (error) {
+        console.log("error creating user file entry");
+        console.log(error);
+        console.log("using default data");
+        this.data = this.defaultData();
+        return this.data;
+      }
+    }
+
+    // check to see if the file exist on disk
     try {
       f = await dataFolder.getEntry(this.fileName);
-      this.file = f;
     } catch (error) {
+      console.log("error getting user data file entry");
       console.log(error);
       console.log("using default user data");
       this.data = this.defaultData;
-      return;
+      return this.data;
     }
 
+    // try to read the file data
     try {
       const fileData = await f.read({ format: storage.formats.utf8 });
       this.data = JSON.parse(fileData);
       console.log("user data file loaded:", this.data);
+      this.data;
     } catch (error) {
-      this.data = this.defaultData;
+      console.log("error reading user data file");
       console.log(error);
-      // TODO: add custom dialog that offers to open the path
-      await alertDialog(
-        "User Data Error",
-        null,
-        "There was an error reading your user data file."
-      );
-      return;
+      console.log("using default user data");
+      this.data = this.defaultData;
+
+      // backup user data file for inspection
+      const backupFilePath = await this.backup();
+
+      if (backupFilePath != null) {
+        // TODO: add custom dialog
+        await alertDialog(
+          "User Data Error",
+          null,
+          "There was an error reading your user data file. A backup was created at the following location.\n" +
+            backupFilePath
+        );
+      }
+
+      return this.data;
     }
   }
 
+  /**
+   * Write user data to disk.
+   * @returns {storage.<File>}
+   */
   async write() {
     const dataFolder = await fs.getDataFolder();
     console.log("writing user data:", dataFolder.nativePath);
 
     let f = this.file;
 
-    try {
-      if (f === undefined) {
+    // if file property is undefined, create the an entry
+    if (f === undefined) {
+      try {
         f = await dataFolder.createEntry(this.fileName, {
           type: storage.types.file,
           overwrite: true,
         });
+        this.file = f;
+      } catch (error) {
+        console.log("error creating user file entry");
+        console.log(error);
+
+        // TODO: add custom dialog
+        await alertDialog(
+          "User Data Error",
+          null,
+          "There was an error creating your user data file."
+        );
+        return f;
       }
-      const fileData = JSON.stringify(this.data);
-      await f.write(fileData, { append: false });
+    }
+
+    // write the user data to the file
+    try {
+      const data = JSON.stringify(this.data);
+      await f.write(data, { append: false });
     } catch (error) {
+      console.log("error writing user data file");
       console.log(error);
-      // TODO: add custom dialog that offers to open the path
+      // TODO: add custom dialog
       await alertDialog(
         "User Data Error",
         null,
         "There was an error writing your user data file."
       );
-      return;
+      return f;
     }
+  }
+
+  /**
+   * Backup the user data file.
+   * @returns {string|null} File path of the backup file.
+   */
+  async backup() {
+    const dataFolder = await fs.getDataFolder();
+    console.log("backing up user data:");
+
+    let f = this.file;
+    let backupFile;
+
+    // check if file entry is set
+    if (f === undefined) {
+      return null;
+    }
+
+    try {
+      await dataFolder.renameEntry(f, f.name + ".bak");
+      console.log("user data file backed up to:", f.nativePath);
+    } catch (error) {
+      console.log("error creating user data backup file");
+      console.log(error);
+      await alertDialog(
+        "User Data Error",
+        null,
+        "There was an error backing up your user data file."
+      );
+    }
+
+    return f.nativePath;
   }
 }
 
